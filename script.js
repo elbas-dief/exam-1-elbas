f607655e87191893bccbc395719bbe5181b108cd
Elbas <haka.elbas@gmail.com>
2026-07-31 00:03:50 +0700

    Move

---
+++ script.js
@@ -0,0 +1,13 @@
+const burger = document.getElementById(`budger-menu`);
+const menu = document.getElementById(`nav-menu`);
+const tes = document.getElementById(`test`);
+
+burger.addEventListener(`click`, () => {    
+    // menu.classList.toggle(`active`);
+    alert(`coba`);
+})
+
+// tes.addEventListener(`click`, ()=>{
+//     alert('Coba!');
+//     // menu.classList.toggle(`active`);
+// })
